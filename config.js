ab4f0dd4aba88416a5a551e63b8f9c95d3f61a64
zecco75 <ruzomaisaac19@gmail.com>
2025-06-06 22:39:55 +0200

    Update config.js

--- config.js
+++ config.js
@@ -41,6 +41,6 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'Put_Your_Session_ID', // Put your session id here ( after linked check your number and get session id it need start with Manu-MD& ) 🤍
+SESSION_ID: process.env.SESSION_ID || 'Manu-MD&qRF1BZzZ#Idr_StR3LVWgSL_o1_FPMqhbFt_0MNX4eaePIHywM2A', // Put your session id here ( after linked check your number and get session id it need start with Manu-MD& ) 🤍
 PORT: process.env.PORT || '3000'
 };
